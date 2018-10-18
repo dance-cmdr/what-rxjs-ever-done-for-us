ba949a34dcc7d71f4e6e1be5b09bab73bbc69027
Alexandros Spyropoulos <alex.spyropoulos@dazn.com>
2018-10-18 17:21:57 +0100

    Improve Tahnks slide

--- src/slides/conclusion/Thanks.js
+++ src/slides/conclusion/Thanks.js
@@ -28,19 +28,27 @@ import {
       return (
             <Slide>
                 <Heading> Thanks !!!</Heading>
-                <Link textColor={'#ffffff'} href={'https://twitter.com/dance_cmdr'}>
-                    <Image src={'./assets/tweeterProfile.png'} height={300}/>
-                    Follow me on Tweet @dance_cmdr
-                </Link>
-                <Link textColor={'#ffffff'} href={'https://engineering.dazn.com'}> 
-                    <StyledLogo src={'./assets/dazn.png'} height={300}/>
-                    Visit https://engineering.dazn.com for news from our Engineering Team.
-                </Link>
+                <Layout>
+                    <Link fit textColor={'#ffffff'} href={'https://twitter.com/dance_cmdr'}>
+                        <Image src={'./assets/tweeterProfile.png'} height={300}/>
+                        Follow me on Tweet @dance_cmdr
+                    </Link>
+                    
+                    <Link fill href={'https://engineering.dazn.com'}> 
+                            <StyledLogo src={'./assets/dazn.png'} height={300} />
+                            <Text textSize={"1em"} textColor={'#ffffff'} >
+                                Visit engineering.dazn.com
+                            </Text>
+                            <Text textSize={"1em"} textColor={'#ffffff'} >
+                                for news from DAZN Engineering Team.
+                            </Text>
+                    </Link>
 
-                <Link textColor={'#ff8888'} href={'https://www.instagram.com/buffy.the.bull.terrier/'}>
-                    <StyledLogo src={'./assets/buffy.gif'} height={300}/>
-                    ... Or Follow buffy.the.bull.terrier on instagram
-                </Link>
+                    <Link textColor={'#ff8888'} href={'https://www.instagram.com/buffy.the.bull.terrier/'}>
+                        <StyledLogo src={'./assets/buffy.gif'} height={300}/>
+                        ... Or Follow buffy.the.bull.terrier on instagram
+                    </Link>
+                </Layout>
             </Slide>
         )
     }
